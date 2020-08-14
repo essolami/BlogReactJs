b2e9e3730c3bd347df90a7685a0bd3fb89cf2881
unknown <essolamih@gmail.com>
2020-08-14 17:04:54 +0100

    Updates

---
+++ precache-manifest.8bf8f69eb627c0f4ea4e8acd9f9696aa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a03421c53ee91164ec07610d824643a",
+    "url": "/BlogReactJs/index.html"
+  },
+  {
+    "revision": "7e4147b6f2946a361c1b",
+    "url": "/BlogReactJs/static/css/main.a554d649.chunk.css"
+  },
+  {
+    "revision": "8c40cecbde6bbe2ee8a7",
+    "url": "/BlogReactJs/static/js/2.b9cdb5f2.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/BlogReactJs/static/js/2.b9cdb5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4147b6f2946a361c1b",
+    "url": "/BlogReactJs/static/js/main.f3bd3708.chunk.js"
+  },
+  {
+    "revision": "e2823a8b81fd35a8cc94",
+    "url": "/BlogReactJs/static/js/runtime-main.eb0eaff7.js"
+  },
+  {
+    "revision": "2bffb99d56fd0cbb72bc3258e72350ff",
+    "url": "/BlogReactJs/static/media/1.2bffb99d.jpg"
+  },
+  {
+    "revision": "d28c13773829784560385d2d91f9bf06",
+    "url": "/BlogReactJs/static/media/12.d28c1377.jpg"
+  },
+  {
+    "revision": "287246a5230c13f2fea68887c950c7f2",
+    "url": "/BlogReactJs/static/media/13.287246a5.jpg"
+  },
+  {
+    "revision": "5ba0d83e77a584112d2de8d7a0ec00d5",
+    "url": "/BlogReactJs/static/media/2.5ba0d83e.jpg"
+  },
+  {
+    "revision": "04dcd8cd5e93508cdfb8662e0ea2fd6f",
+    "url": "/BlogReactJs/static/media/3.04dcd8cd.jpg"
+  },
+  {
+    "revision": "740d3cce3827176f8b1ab24624c1bd1a",
+    "url": "/BlogReactJs/static/media/4.740d3cce.jpg"
+  },
+  {
+    "revision": "e69a6f141accc3580d3082826563062e",
+    "url": "/BlogReactJs/static/media/5.e69a6f14.jpg"
+  },
+  {
+    "revision": "0c77b02a372570e26bd09871839015bb",
+    "url": "/BlogReactJs/static/media/6.0c77b02a.jpg"
+  },
+  {
+    "revision": "24f89814882c1cf546666a5786b7ef17",
+    "url": "/BlogReactJs/static/media/7.24f89814.jpg"
+  },
+  {
+    "revision": "c9e2c771ae8b5a3f7877b3c5025653c2",
+    "url": "/BlogReactJs/static/media/8.c9e2c771.jpg"
+  },
+  {
+    "revision": "342fe99b933c6ec6c25694a8c4fad973",
+    "url": "/BlogReactJs/static/media/9.342fe99b.jpg"
+  },
+  {
+    "revision": "cb48b4d093d1f3be6a7f608d9a10461d",
+    "url": "/BlogReactJs/static/media/aboutus.cb48b4d0.jpg"
+  },
+  {
+    "revision": "34a7bd0039ef314f0418d2d18686bcf9",
+    "url": "/BlogReactJs/static/media/add2.34a7bd00.png"
+  },
+  {
+    "revision": "5ff227e8c3d9a372bc671bef9b7e89c1",
+    "url": "/BlogReactJs/static/media/ff.5ff227e8.jpg"
+  },
+  {
+    "revision": "58d0fda3d12c2573a91ac37fa6c1c4bd",
+    "url": "/BlogReactJs/static/media/outpout.58d0fda3.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BlogReactJs/precache-manifest.8bf8f69eb627c0f4ea4e8acd9f9696aa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BlogReactJs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
